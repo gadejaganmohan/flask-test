716eaa5594f9895cb723dbd2cb8fb00f2d91499e
nani <gadejaganmohan@gmail.com>
2024-05-28 17:58:44 +0530

    added index and result pages

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "Its working MF!"
+    return render_template('index.html')
 
 @app.route("/next")
 def next():
-    return render_template('index.html')
+    return render_template('result.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
